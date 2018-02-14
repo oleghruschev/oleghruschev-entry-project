93ad45077b50996e50a1476db3f0506838205ec4
oleg <hruschev.oleg@mail.ru>
2018-02-14 09:26:14 +0500

    исправил grunfile.js

--- gruntfile.js
+++ gruntfile.js
@@ -1,61 +1,42 @@
 module.exports = function(grunt) {
-+ 
-+  grunt.initConfig({
-+      concat: {
-+          main: {
-+              src: [
-+                  'node_modules/backbone/backbone.js',
-+                  'node_modules/jquery/dist/jquery.js',
-+                  'node_modules/lodash/lodash.js'
-+              ],
-+              dest: 'build/scripts.js'
-+          }
-+
-+      },
-+      concat: {
-+          main: {
-+              src: [
-+                  //
-+              ],
-+              dest: 'build/constructor.js'
-+          }
-+
-+
-+      },
-+      concat: {
-+          main: {
-+              src: [
-+                  //
-+              ],
-+              dest: 'build/app.js'
-+          }
-+      },
-+
-+      sass: {
-+          dist: {
-+              options: {
-+                  style: 'compressed'
-+              },
-+              files: {
-+                  'css/style.css': 'css/style.scss'
-+              }
-+          }
-+      }
-+
-+      watch: {
-+          css: {
-+              files: ['css/*.scss'],
-+              tasks: ['sass'],
-+              options: {
-+                  spawn: false,
-+              }
-+          }
-+      }
-+  })
-+};
-+
-+grunt.loadNpmTasks('grunt-contrib-concat');
-+grunt.loadNpmTasks('grunt-contrib-sass');
-+grunt.loadNpmTasks('grunt-contrib-watch');
 
-+grunt.registerTask('default', ['concat', 'sass', 'watch']);
+  grunt.initConfig({
+      concat: {
+          libs: {
+              src: [
+                  'node_modules/backbone/backbone.js',
+                  'node_modules/jquery/dist/jquery.js',
+                  'node_modules/lodash/lodash.js'
+              ],
+              dest: 'build/libs.js'
+          }
+      },
+
+      sass: {
+          dist: {
+              options: {
+                  style: 'compressed'
+              },
+              files: {
+                  'css/style.css': 'css/style.scss'
+              }
+          }
+      },
+
+      watch: {
+          css: {
+              files: ['css/*.scss'],
+              tasks: ['sass'],
+              options: {
+                  spawn: false,
+              }
+          }
+      }
+  });
+};
+
+grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-sass');
+grunt.loadNpmTasks('grunt-contrib-watch');
+
+grunt.registerTask('default', ['concat', 'sass', 'watch']);
