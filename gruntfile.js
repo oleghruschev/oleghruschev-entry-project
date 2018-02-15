24ffe2ce0ac9839fdd2806461d24079487c59e0c
Oleg <hruschev.oleg@mail.ru>
2018-02-15 21:35:26 +0500

    добавил пустую строку в файлы

--- gruntfile.js
+++ gruntfile.js
@@ -42,7 +42,7 @@ module.exports = function(grunt) {
               tasks: ['sass'],
               options: {
                   spawn: false
-               }
+              }
           }
       }
   });
