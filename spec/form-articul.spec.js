58af002941654341dd089179b51f588df890fa05
oleg <hruschev.oleg@mail.ru>
2018-02-19 10:49:26 +0500

    снова исправил файлы

--- spec/form-articul.spec.js
+++ spec/form-articul.spec.js
@@ -22,29 +22,3 @@ describe("form-articul", function() {
         expect($('.modal_wrap')).toHaveClass('modal_close');
     });
 });
-
-describe("form-articul", function() {
-
-    $('body').append(
-        '<div class="container">' +
-            '<div class="modal_wrap"><span class="close"></span></div>' +
-        '</div>'
-    );
-
-    const formArticul = new FormArticul();
-
-    it("test", function() {
-        expect(formArticul.el).toHaveClass('container');
-    });
-
-    it("open the form", function() {
-        $('.append_btn').trigger('click');
-        expect($('.modal_wrap')).not.toHaveClass('modal_close');
-    });
-
-    it("close the form", function() {
-        $('.close').trigger('click');
-        expect($('.modal_wrap')).toHaveClass('modal_close');
-    });
-});
-
--- src/constructors/products-model.js
+++ src/constructors/products-model.js
@@ -1,4 +1,3 @@
 const ProductsModel = Backbone.Model.extend({
   url: "https://www.sima-land.ru/api/v3/item/"
 });
-
